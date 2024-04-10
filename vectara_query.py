17fc5b0a4e70419487330cdf1f2466bc70e9e20a
Aryan Poonacha <poonacharyan@gmail.com>
2024-04-09 23:22:40 -0400

    Added streamlit frontend, completed v1 of entire pipeline

--- vectara_query.py
+++ vectara_query.py
@@ -1,85 +1,147 @@
 import os
+import json
+import requests
 from dotenv import load_dotenv
 from Vectara.query import *
-import requests
 
-load_dotenv()
-
-url = "https://api.vectara.io/v1/query"
-query_prompt = 'Who is the Dragon of Dojima?'
-
-VECTARA_CUSTOMER_ID = os.getenv('VECTARA_CUSTOMER_ID')
-VECTARA_CORPUS_ID  = os.getenv('VECTARA_CORPUS_ID')
-
-#approach 1
-
-# VECTARA_JWT_TOKEN  = os.getenv('VECTARA_JWT_TOKEN')
-# response = query_vectara(VECTARA_CUSTOMER_ID, VECTARA_CORPUS_ID, url, VECTARA_JWT_TOKEN, query_prompt)
-# print(response)
-
-
-#approach 2
-VECTARA_YAKUZA_WIKI_API_KEY = os.getenv('VECTARA_YAKUZA_WIKI_API_KEY')
-
-payload = json.dumps({
-  "query": [
-    {
-      "query": query_prompt,
-      "start": 0,
-      "numResults": 10,
-      "contextConfig": {
-        "sentencesBefore": 3,
-        "sentencesAfter": 3,
-        "startTag": "<b>",
-        "endTag": "</b>"
-      },
-      "corpusKey": [
-        {
-          "corpusId": VECTARA_CORPUS_ID,
-          "semantics": "DEFAULT",
-          "dim": [
-            {
-              "name": "relevance",
-              "weight": 1.5
-            }
-          ],
-          "lexicalInterpolationConfig": {
-            "lambda": 0.7
-          }
+class VectaraAPI:
+    def __init__(self):
+        load_dotenv()
+        self.url = "https://api.vectara.io/v1/query"
+        self.customer_id = os.getenv('VECTARA_CUSTOMER_ID')
+        self.corpus_id = os.getenv('VECTARA_CORPUS_ID')
+        self.api_key = os.getenv('VECTARA_YAKUZA_WIKI_API_KEY')
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'customer-id': self.customer_id,
+            'x-api-key': self.api_key
         }
-      ],
-      "rerankingConfig": {
-        "rerankerId": 272725718,
-        "mmrConfig": {
-          "diversityBias": 0.4
-        }
-      },
-      "summary": [
-        {
-          "summarizerPromptName": "vectara-summary-ext-v1.3.0",
-          "maxSummarizedResults": 3,
-          "responseLang": "en",
-          "promptText": "[\n  {\"role\": \"system\", \"content\": \"You are an expert in summarizing information from the Yakuza video game series.\"},\n  #foreach ($result in $vectaraQueryResults)\n    {\"role\": \"user\", \"content\": \"What are the key points in result number $vectaraIdxWord[$foreach.index]?\"},\n    {\"role\": \"assistant\", \"content\": \"In result number $vectaraIdxWord[$foreach.index], the key points are: ${result.getText()}\"},\n  #end\n  {\"role\": \"user\", \"content\": \"Can you generate a comprehensive summary that answers the question?\"}\n]\n",
-          "responseChars": 500,
-          "modelParams": {
-            "maxTokens": 1024,
-            "temperature": 0.6,
-            "frequencyPenalty": 0.2,
-            "presencePenalty": 0.1
-          },
-          "factualConsistencyScore": True
-        }
-      ]
-    }
-  ]
-})
-headers = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json',
-  'customer-id': VECTARA_CUSTOMER_ID,
-  'x-api-key': VECTARA_YAKUZA_WIKI_API_KEY
-}
+        self.conversation_id = ""
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    def query(self, query_prompt):
+        payload = json.dumps({
+          "query": [
+                    {
+                      "query": query_prompt,
+                      "start": 0,
+                      "numResults": 10,
+                      "contextConfig": {
+                        "sentencesBefore": 3,
+                        "sentencesAfter": 3,
+                        "startTag": "<b>",
+                        "endTag": "</b>"
+                      },
+                      "corpusKey": [
+                        {
+                          "corpusId": self.corpus_id,
+                          "semantics": "DEFAULT",
+                          "dim": [
+                            {
+                              "name": "relevance",
+                              "weight": 1.5
+                            }
+                          ],
+                          "lexicalInterpolationConfig": {
+                            "lambda": 0.7
+                          }
+                        }
+                      ],
+                      "rerankingConfig": {
+                        "rerankerId": 272725718,
+                        "mmrConfig": {
+                          "diversityBias": 0.4
+                        }
+                      },
+                      "summary": [
+                        {
+                          "summarizerPromptName": "vectara-summary-ext-v1.2.0",
+                          "maxSummarizedResults": 3,
+                          "responseLang": "en",
+                          #"promptText": "[\n  {\"role\": \"system\", \"content\": \"You are an expert in summarizing information from the Yakuza video game series.\"},\n  #foreach ($result in $vectaraQueryResults)\n    {\"role\": \"user\", \"content\": \"What are the key points in result number $vectaraIdxWord[$foreach.index]?\"},\n    {\"role\": \"assistant\", \"content\": \"In result number $vectaraIdxWord[$foreach.index], the key points are: ${result.getText()}\"},\n  #end\n  {\"role\": \"user\", \"content\": \"Can you generate a comprehensive summary that answers the question?\"}\n]\n",
+                        }
+                      ]
+                    }
+                  ]
+        })
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        return json.loads(response.text)
 
-print(response.text)
+    def save_responses(self, response_json, save_to_file = False):
+        top_responses = {resp['text']: resp['score'] for resp in response_json['responseSet'][0]['response']}
+        summary_text = response_json['responseSet'][0]['summary'][0]['text']
+        if save_to_file is True:
+          with open('response_data.txt', 'w', encoding="utf-8") as f:
+              f.write("Top Responses:\n")
+              for text, score in top_responses.items():
+                  f.write(f"Text: {text}, Score: {score}\n")
+              f.write("\nSummary:\n")
+              f.write(summary_text)
+        return summary_text, top_responses
+
+    def chat(self, query_prompt, conversation_id=""):
+        if conversation_id != "":
+            convo_id = conversation_id
+        else:
+            convo_id = self.conversation_id
+        payload = json.dumps({
+                      "query": [
+                    {
+                      "query": query_prompt,
+                      "start": 0,
+                      "numResults": 10,
+                      "contextConfig": {
+                        "sentencesBefore": 3,
+                        "sentencesAfter": 3,
+                        "startTag": "<b>",
+                        "endTag": "</b>"
+                      },
+                      "corpusKey": [
+                        {
+                          "corpusId": self.corpus_id,
+                          "semantics": "DEFAULT",
+                          "dim": [
+                            {
+                              "name": "relevance",
+                              "weight": 1.5
+                            }
+                          ],
+                          "lexicalInterpolationConfig": {
+                            "lambda": 0.7
+                          }
+                        }
+                      ],
+                      "rerankingConfig": {
+                        "rerankerId": 272725718,
+                        "mmrConfig": {
+                          "diversityBias": 0.4
+                        }
+                      },
+                      "summary": [
+                          {
+                              "chat": {
+                                  "store": True,
+                                  "conversationId": convo_id
+                              },
+                              "maxSummarizedResults": 3,
+                              "responseLang": "en",
+                          }
+                      ]
+                    }
+                  ]
+        })
+
+        response = requests.request("POST", self.url, headers=self.headers, data=payload)
+        response_json = json.loads(response.text)
+        if not self.conversation_id:
+            self.conversation_id = response_json['responseSet'][0]['summary'][0]['chat']['conversationId']
+        return response_json
+
+# Usage
+# vectara_api = VectaraAPI()
+# query_prompt = 'Who is the Dragon of Dojima?'
+# query_prompt2 = 'Who are the daughter and brother of Kiryu??'
+# response_json = vectara_api.query(query_prompt2)
+# vectara_api.save_responses(response_json)
+# conversation_id = ""
+# chat_response_json = vectara_api.chat(conversation_id)
